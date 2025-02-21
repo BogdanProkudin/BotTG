c76ad8cced74b42f84d00ba3aab366c603b97935
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-21 16:12:41 +0200

    laswrewqвbsze1

--- bot.js
+++ bot.js
@@ -425,6 +425,9 @@ bot.onText(/\/noob/, async (msg) => {
   const file_id =
     "AgACAgIAAxkBAAMsZ7hMijU3fEdrNiqMrBQpr1kAAQIvAALm6TEbCd_BSYt1o3L4pQRsAQADAgADbQADNgQ";
   const fileInfo = await bot.getFile(file_id);
+  const fileUrl = `https://api.telegram.org/file/bot${"7510967344:AAE67qW9AbBBu820dL9C8W6saUTQrWoRUAE"}/${
+    fileInfo.file_path
+  }`;
   const imagePath = `./${chatId}.jpg`;
   const response = await axios.get(fileUrl, { responseType: "arraybuffer" });
   const buffer = Buffer.from(response.data);
