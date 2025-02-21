8d6236d2f7b029a77717d2978a87f43258e532f2
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-21 16:27:43 +0200

    laswrewqвbsze1цzzz

--- bot.js
+++ bot.js
@@ -95,10 +95,10 @@ async function processPaymentNotification(req, res) {
     if (!collectionUser) {
       return;
     }
-    console.log("", InvId, collectionUser);
+
     const invIdNumber = Number(InvId);
     const user = await collectionUser.findOne({ invId: invIdNumber });
-    console.log(user, "", InvId);
+
     // Генерация строки для вычисления контрольной суммы
     let additionalParamsString = "";
     if (Object.keys(additionalParams).length > 0) {
@@ -406,38 +406,6 @@ bot.on("location", async (msg) => {
   }
 
   //   bot.sendLocation(chatId, latitude, longitude);
-});
-async function sendBufferImage(chatId, buffer) {
-  const stream = new Readable({
-    read() {
-      this.push(buffer);
-      this.push(null); // Завершаем поток
-    },
-  });
-
-  await bot.sendPhoto(chatId, { source: stream });
-}
-
-bot.onText(/\/noob/, async (msg) => {
-  const chatId = msg.chat.id;
-  const imageUrl =
-    "https://api.telegram.org/file/bot7510967344:AAE67qW9AbBBu820dL9C8W6saUTQrWoRUAE/photos/file_0.jpg";
-  const file_id =
-    "AgACAgIAAxkBAAMsZ7hMijU3fEdrNiqMrBQpr1kAAQIvAALm6TEbCd_BSYt1o3L4pQRsAQADAgADbQADNgQ";
-  const fileInfo = await bot.getFile(file_id);
-
-  const imagePath = `./${chatId}.jpg`;
-  const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
-  const buffer = Buffer.from(response.data);
-
-  // Сохраняем изображение локально (опционально)
-  // fs.writeFileSync(imagePath, buffer);
-
-  // Отправляем изображение в другой чат
-  await bot.sendPhoto(chatId, buffer);
-
-  // Отправляем подтверждение пользователю
-  await bot.sendMessage(chatId, "Ваше фото было успешно переслано!");
 });
 
 bot.onText(/\/start/, async (msg) => {
