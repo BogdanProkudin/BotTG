0e425e9379664dc9bb495fe06765abc5cbc633f7
Богдан Прокудин <prokudinbogdan50@gmail.com>
2025-02-21 12:12:43 +0200

    private232

--- bot.js
+++ bot.js
@@ -1318,7 +1318,7 @@ bot.on("message", async (msg) => {
       text !== "Назад"
     ) {
       const merchantLogin = "Florimnodi";
-      const password1 = "pE4fu3bO2qglZCa3dI5T";
+      const password1 = "mnVhyV7vb3nLt58fAyr3";
       const invId = Math.floor(100000 + Math.random() * 900000);
 
       const outSum = await user.price;
@@ -1355,7 +1355,7 @@ bot.on("message", async (msg) => {
       text !== "Назад"
     ) {
       const merchantLogin = "Florimnodi";
-      const password1 = "pE4fu3bO2qglZCa3dI5T";
+      const password1 = "mnVhyV7vb3nLt58fAyr3";
       const invId = Math.floor(100000 + Math.random() * 900000);
 
       const outSum = await user.price;
